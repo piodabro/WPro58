ab71a4741547f963d89fb91c6006733b01282033
Telekatz <telekatz@gmx.de>
2018-07-29 19:48:02 +0200

    Fix OSD sync switching Add OSD58 schematic

--- PRO58/Src/OSD.cpp
+++ PRO58/Src/OSD.cpp
@@ -1,3 +1,28 @@
+/*
+Copyright (c) 2018 Telekatz
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <spi.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -225,7 +250,7 @@ namespace OSD {
                 uint8_t alpha = 0xff;
 #else
                 uint8_t alpha = 0x00;
-#endif;
+#endif
                 if(videoConnect == 2) {
                     alpha=0x00;
                 }
@@ -322,6 +347,8 @@ namespace OSD {
 
 
     void __attribute__((optimize("Ofast"))) TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+        DMA_Channel_TypeDef* dmaSpiA = OSD_DMA_SPI_A;
+        uint8_t buf = LINEBUFFER_SIZE;
 
 #ifdef DEBUG_OUT
         screenBuffer[24] = '0'+videoConnect;
@@ -379,6 +406,14 @@ namespace OSD {
 #ifdef OSD_CSYNC_IRQ
             HAL_NVIC_DisableIRQ(OSD_CSYNC_IRQ);
 #endif
+            while(buf--) {
+                linebufferA[0][buf] = 0x00;
+            }
+            dmaSpiA->CCR = 0;
+            dmaSpiA->CNDTR = 1;
+            dmaSpiA->CMAR = (uint32_t)linebufferA[0];
+            dmaSpiA->CCR |= DMA_CCR_EN;
+            
             videoConnect = 0;
             palSync = SYNC_COUNTER;
             ntscSync = SYNC_COUNTER;
